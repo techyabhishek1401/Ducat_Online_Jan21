1a42670222d23dec6653641a83fbbef0be6bd13c
techyAbhishek <techyabhishek1401@gmail.com>
2021-03-24 11:04:18 +0530

    React and promises

--- NODE/02-asyncronous/index.js
+++ NODE/02-asyncronous/index.js
@@ -1,4 +1,4 @@
-
+const  fs=require('fs');
 console.log('Starting');
 
 getUser(1,(user)=>{
@@ -11,6 +11,74 @@ function getUser(id,callback){
         callback({id,name:"abhi"})
     },2000);   
 }
+let userRecieved=true;
+const getUserPromise=(id)=>{
+  return  new Promise((resolve,reject)=>{
+        if(userRecieved){
+          setTimeout(()=>{
+              console.log("PROMISE RESOLVED");
+              resolve({id,name:"Success"})
+          },4000)
+        }
+        else {
+            setTimeout(()=>{
+                console.log("PROMISE REJECT");
+               reject("DB ERROR")
+            },4000) 
+        }
+    })
+}
+
+getUserPromise(2).then((user)=>{
+    console.log("user promised:",user);
+}).catch(err=>{
+    console.log("user promised error:",err); 
+})
+
+
+
+const getFile = (fileName) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(fileName,'UTF-8' ,(err, data) => {
+        if (err) {
+          reject(err)  // calling `reject` will cause the promise to fail with or without the error passed as an argument
+          return        // and we don't want to go any further
+        }
+        resolve(data)
+      })
+    })
+  }
+ getFile('./Dummy.txt').then(data=>{
+     console.log("resolve")
+     console.log(data);;
+ }).catch(err=>{
+     console.log("reject")
+     console.error(err);
+ }) 
+
+
+
+
+
+
+
+
+    
+//  let user= (id)=>{
+//       return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             console.log('reading data from db');       
+//             resolve({id,name:"bhola"})
+//         },2000);   
+//       })       
+//     }
+//     user(5).then(data=>console.log("user:",data)).catch(err=>{
+//         console.log("err:",err)
+//     })
+
+
+
+
 
 /* 
 callback,
